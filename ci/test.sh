9f1712e05146e6b3ce9a816674df295385d34902
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-12-08 11:03:59 -0800

    #9: Test correct binaries in CI

--- ci/test.sh
+++ ci/test.sh
@@ -5,7 +5,7 @@ set -x
 
 cd $(dirname "$0")/../build/tests
 if [ "$RUN_TYPE" = "test" ]; then
-   exec ctest -j3 --output-on-failure && ../libraries/vendor/mira/test/mira_test
+   exec ctest -j3 --output-on-failure
 elif [ "$RUN_TYPE" = "coverage" ]; then
-   exec valgrind --error-exitcode=1 --leak-check=yes ./koinos_tests
+   exec valgrind --error-exitcode=1 --leak-check=yes ./koinos_state_db_tests
 fi
