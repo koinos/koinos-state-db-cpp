239239b576a339ee468677df4626f5b84e1fea06
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-04 11:32:38 -0800

    Refactor for cmake submodule

--- libraries/state_db/include/koinos/state_db/backends/types.hpp
+++ libraries/state_db/include/koinos/state_db/backends/types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 namespace koinos::state_db::backends::detail {
--- tests/tests/main.cpp
+++ tests/tests/main.cpp
@@ -1,3 +1,3 @@
 #define BOOST_TEST_MODULE koinos_state_db_tests
-#include <BoostTestTargetConfig.h>
+#include <boost/test/unit_test.hpp>
 #include <boost/test/included/unit_test.hpp>
