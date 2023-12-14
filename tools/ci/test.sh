b5cc7a5d59c45221c6594ddfea5e835730d3b8c9
Steve Gerbino <steve@gerbino.co>
2023-12-14 10:11:58 -0500

    Cleanup State DB CMake issues

--- tools/ci/test.sh
+++ tools/ci/test.sh
@@ -3,12 +3,13 @@
 set -e
 set -x
 
-cd $TRAVIS_BUILD_DIR/build/tests
 if [ "$RUN_TYPE" = "test" ]; then
-   exec ctest -j3 --output-on-failure
-
    cd $TRAVIS_BUILD_DIR/build
-   cmake .. build . --config Release --parallel 3 --target format.check
+   cmake --build . --config Release --parallel 3 --target format.check
+
+   cd $TRAVIS_BUILD_DIR/build/tests
+   exec ctest -j3 --output-on-failure
 elif [ "$RUN_TYPE" = "coverage" ]; then
+   cd $TRAVIS_BUILD_DIR/build/tests
    exec valgrind --error-exitcode=1 --leak-check=yes ./koinos_state_db_tests
 fi
